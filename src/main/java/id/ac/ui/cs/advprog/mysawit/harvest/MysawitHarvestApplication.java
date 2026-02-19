5f828b140084e221af05dbd67550fc11c2627bbd
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2026-02-20 05:20:21 +0700

    chore: init mysawit-harvest from template

--- src/main/java/id/ac/ui/cs/advprog/mysawit/harvest/MysawitHarvestApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/harvest/MysawitHarvestApplication.java
@@ -1,13 +1,13 @@
-package id.ac.ui.cs.advprog.mysawit.auth;
+package id.ac.ui.cs.advprog.mysawit.harvest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MysawitAuthApplication {
+public class MysawitHarvestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MysawitAuthApplication.class, args);
+        SpringApplication.run(MysawitHarvestApplication.class, args);
     }
 
 }
