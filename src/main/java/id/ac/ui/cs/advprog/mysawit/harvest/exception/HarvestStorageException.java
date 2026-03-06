585ba5ce17ea2215bc788b00879322e9c3c09a7e
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2026-03-06 14:26:53 +0700

    feat: add Harvest exception

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/harvest/exception/HarvestStorageException.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.mysawit.harvest.exception;
+
+public class HarvestStorageException extends RuntimeException {
+    public HarvestStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
