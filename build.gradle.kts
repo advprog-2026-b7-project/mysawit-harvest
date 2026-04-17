4e941a2a47a2103b50dd9e31c9fa8125a827d735
rafsandeylora <rafsanjanirafsanjani8@gmail.com>
2026-04-17 14:30:23 +0700

    revert

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.sonarqube") version "4.4.1.3373"
+
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -17,53 +18,19 @@ java {
     }
 }
 
-configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
-}
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    val lombokVersion = "1.18.38"
-
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    compileOnly("org.projectlombok:lombok:$lombokVersion")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
-    
-    runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("com.h2database:h2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.h2database:h2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.register<Test>("unitTest") {
-    description = "Runs unit tests."
-    group = "verification"
-
-    filter {
-        excludeTestsMatching("*FunctionalTest")
-    }
-}
-
-tasks.register<Test>("functionalTest") {
-    description = "Runs functional tests."
-    group = "verification"
-
-    filter {
-        includeTestsMatching("*FunctionalTest")
-    }
-}
-
-tasks.withType<Test>().configureEach {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
 
@@ -71,13 +38,11 @@ tasks.test {
     filter {
         excludeTestsMatching("*FunctionalTest")
     }
-
-    finalizedBy(tasks.jacocoTestReport)
+    finalizedBy(tasks.named("jacocoTestReport"))
 }
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
-
     reports {
         xml.required.set(true)
         html.required.set(true)
--- src/main/java/id/ac/ui/cs/advprog/mysawit/harvest/model/HarvestStatus.java
+++
@@ -1,7 +0,0 @@
-package id.ac.ui.cs.advprog.mysawit.harvest.model;
-
-public enum HarvestStatus {
-    PENDING,
-    APPROVED,
-    REJECTED
-}
